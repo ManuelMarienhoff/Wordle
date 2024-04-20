f8214eca525bdf644db9fdd634c8cb339357785b
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-04-20 17:39:01 -0300

    moving key clicking functions to App.jsx to use them globaly

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,67 @@ import { createContext } from 'react'; //allows me to create a global context fo
 export const AppContext = createContext();
 
 function App() {
+  /* ****************Board and currentAttempt******************** */
   const [board, setBoard] = useState(defaultBoard);
   const [currentAttempt, setCurrentAttempt] = useState({
     attempt: 0,
     letterPosition: 0,
   });
 
+  /* *******************************PRESS KEY FUNCTION************************** */
+  const onSelectedLetter = (keyVal) => {
+    if (currentAttempt.letterPosition > 4)
+      return; /* can't add new letter if selected keyBox is the last one of the row, end function. */
+
+    /* on click, insert letter in keyBox */
+    const newBoard = [...board]; /* read the status of the board */
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] =
+      keyVal; /* insert letter on selected keyBox */
+    setBoard(newBoard);
+    setCurrentAttempt({
+      /* move onto the next keyBox after inserting the letter */
+      ...currentAttempt,
+      letterPosition: currentAttempt.letterPosition + 1,
+    });
+  };
+  /* *************************DELETE KEY FUNCTION************************* */
+  const onDelete = () => {
+    if (currentAttempt.letterPosition === 0)
+      return; /* can't delete if selected keyBox is first box / empty, end function */
+    const newBoard = [...board]; /* read the status of the board */
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] =
+      ''; /* delete letter on previous keyBox  */
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currentAttempt,
+      letterPosition: currentAttempt.letterPosition - 1,
+    }); /* go back to deleted keyBox */
+  };
+  /* ******************** ENTER KEY FUNCTION ********************************/
+  const onEnter = () => {
+    if (currentAttempt.letterPosition !== 5)
+      return; /* if not on last box of row, end function */
+    setCurrentAttempt({
+      attempt: currentAttempt.attempt + 1,
+      letterPosition: 0 /* move to the next row down below */,
+    });
+  };
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider
-        value={{ board, setBoard, currentAttempt, setCurrentAttempt }}>
+      <AppContext.Provider /* sharing values globaly with the app */
+        value={{
+          board,
+          setBoard,
+          currentAttempt,
+          setCurrentAttempt,
+          onSelectedLetter,
+          onDelete,
+          onEnter,
+        }}>
         {/* everything inside this provider can access to the values*/}
         <div className="game">
           <Board />
--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,42 +2,16 @@ import { useContext } from 'react'; /* needs to be imported in each file I want 
 import { AppContext } from '../App';
 
 function Key({ keyVal, bigKey }) {
-  const { board, setBoard, currentAttempt, setCurrentAttempt } =
-    useContext(AppContext);
+  const { onSelectedLetter, onDelete, onEnter } =
+    useContext(AppContext); /* import global values from context */
 
   const selectLetter = () => {
     if (keyVal === 'ENTER') {
-      if (currentAttempt.letterPosition !== 5)
-        return; /* if not on last box of row, end function */
-      setCurrentAttempt({
-        attempt: currentAttempt.attempt + 1,
-        letterPosition: 0 /* move to the next row down below */,
-      });
+      onEnter();
     } else if (keyVal === 'DELETE') {
-      if (currentAttempt.letterPosition === 0)
-        return; /* can't delete if selected box is 0/empty, end function */
-      const newBoard = [...board]; /* read the status of the board */
-      newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] =
-        ''; /* delete letter on previous keyBox  */
-      setBoard(newBoard);
-      setCurrentAttempt({
-        ...currentAttempt,
-        letterPosition: currentAttempt.letterPosition - 1,
-      }); /* go back to deleted keyBox */
+      onDelete();
     } else {
-      if (currentAttempt.letterPosition > 4)
-        return; /* If selected keyBox is the last one of the row, end function */
-
-      /* on click, insert letter in keyBox */
-      const newBoard = [...board]; /* read the status of the board */
-      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] =
-        keyVal; /* insert letter on selected keyBox */
-      setBoard(newBoard);
-      setCurrentAttempt({
-        /* move onto the next keyBox after inserting the letter */
-        ...currentAttempt,
-        letterPosition: currentAttempt.letterPosition + 1,
-      });
+      onSelectedLetter(keyVal);
     }
   };
   return (
