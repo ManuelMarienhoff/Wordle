e5f432c3a2a590b149f4dbfb309fec7ccd7213e6
ManuelMarienhoff <manuelmarienhoff1@gmail.com>
2024-04-21 00:53:52 -0300

    fixes CSS

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -46,40 +46,42 @@ function Keyboard() {
 
   return (
     <div className="keyboard" onKeyDown={handleKeyboard}>
-      <div className="line1">
-        {keys1.map((key) => {
-          return (
-            <Key
-              keyVal={key}
-              key={key + 'key'}
-              disabled={disabledLetters.includes(key)}
-            />
-          );
-        })}
-      </div>
-      <div className="line2">
-        {keys2.map((key) => {
-          return (
-            <Key
-              keyVal={key}
-              key={key + 'key'}
-              disabled={disabledLetters.includes(key)}
-            />
-          );
-        })}
-      </div>
-      <div className="line3">
-        <Key keyVal={'ENTER'} key={'ENTER' + 'key'} bigKey />
-        {keys3.map((key) => {
-          return (
-            <Key
-              keyVal={key}
-              key={key + 'key'}
-              disabled={disabledLetters.includes(key)}
-            />
-          );
-        })}
-        <Key keyVal={'DELETE'} key={'DELETE' + 'key'} bigKey />
+      <div className="keyRowContainer">
+        <div className="line1">
+          {keys1.map((key) => {
+            return (
+              <Key
+                keyVal={key}
+                key={key + 'key'}
+                disabled={disabledLetters.includes(key)}
+              />
+            );
+          })}
+        </div>
+        <div className="line2">
+          {keys2.map((key) => {
+            return (
+              <Key
+                keyVal={key}
+                key={key + 'key'}
+                disabled={disabledLetters.includes(key)}
+              />
+            );
+          })}
+        </div>
+        <div className="line3">
+          <Key keyVal={'ENTER'} key={'ENTER' + 'key'} bigKey />
+          {keys3.map((key) => {
+            return (
+              <Key
+                keyVal={key}
+                key={key + 'key'}
+                disabled={disabledLetters.includes(key)}
+              />
+            );
+          })}
+          <Key keyVal={'DELETE'} key={'DELETE' + 'key'} bigKey />
+        </div>
       </div>
     </div>
   );
